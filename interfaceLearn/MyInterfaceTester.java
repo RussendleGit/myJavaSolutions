867961a6e816f1293bc24e3585aca87b0d48cbe6
RussendleGit <92649392+RussendleGit@users.noreply.github.com>
2024-09-07 13:13:27 -0600

    Add files via upload
    MyInterfaceTester is the main file. MyInterface shows what the interface is supposed to so. And Laptop is the interface I created. These are horrible names lol

---
+++ interfaceLearn/MyInterfaceTester.java
@@ -0,0 +1,8 @@
+class MyInterfaceTester {
+    public static void main(String[] args) {
+        Laptop myLaptop = new Laptop();
+        System.out.println(myLaptop.keyPressed());
+        System.out.println(myLaptop.keyNumber());
+
+    }
+} 
